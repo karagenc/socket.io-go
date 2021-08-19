07fb4f3ae5c67f5de1b09b23f9278e1adbed22f7
tomruk <41700170+tomruk@users.noreply.github.com>
2021-08-19 16:33:40 +0300

    Remove some reserved events
    They are redundant and not used by the official Socket.IO implementation.

--- reserved_events.go
+++ reserved_events.go
@@ -1,21 +1,12 @@
 package sio
 
 var reservedEvents = map[string]bool{
-	"error":             true,
-	"ping":              true,
-	"pong":              true,
-	"connect":           true,
-	"connect_error":     true,
-	"connect_timeout":   true,
-	"connecting":        true,
-	"disconnect":        true,
-	"disconnecting":     true,
-	"reconnect":         true,
-	"reconnect_attempt": true,
-	"reconnect_failed":  true,
-	"reconnect_error":   true,
-	"newListener":       true,
-	"removeListener":    true,
+	"connect":        true,
+	"connect_error":  true,
+	"disconnect":     true,
+	"disconnecting":  true,
+	"newListener":    true,
+	"removeListener": true,
 }
 
 func IsEventReserved(eventName string) bool {
