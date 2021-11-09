fe7aacbec85019a32d98f8dd5f0617cbfe05d644
tomruk <41700170+tomruk@users.noreply.github.com>
2021-11-09 10:11:01 +0300

    Add serverConn

---
+++ server_conn.go
@@ -0,0 +1,136 @@
+package sio
+
+import (
+	"sync"
+
+	eio "github.com/tomruk/socket.io-go/engine.io"
+	eioparser "github.com/tomruk/socket.io-go/engine.io/parser"
+	"github.com/tomruk/socket.io-go/parser"
+)
+
+type serverSocketStore struct {
+	sockets map[string]*serverSocket
+	mu      sync.Mutex
+}
+
+func newServerSocketStore() *serverSocketStore {
+	return &serverSocketStore{
+		sockets: make(map[string]*serverSocket),
+	}
+}
+
+func (s *serverSocketStore) Get(sid string) (ss *serverSocket, ok bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	ss, ok = s.sockets[sid]
+	return
+}
+
+func (s *serverSocketStore) GetAll() (sockets []*serverSocket) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	sockets = make([]*serverSocket, len(s.sockets))
+	i := 0
+	for _, ss := range s.sockets {
+		sockets[i] = ss
+		i++
+	}
+	return
+}
+
+func (s *serverSocketStore) Add(ss *serverSocket) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.sockets[ss.ID()] = ss
+}
+
+func (s *serverSocketStore) Delete(sid string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.sockets, sid)
+}
+
+// This struct represents a connection to the server.
+//
+// This is the equivalent of the Client class at: https://github.com/socketio/socket.io/blob/4.3.2/lib/client.ts#L21
+type serverConn struct {
+	id  string
+	eio eio.Socket
+
+	sockets *serverSocketStore
+
+	// This mutex is used for protecting parser from concurrent calls.
+	// Due to the modular and concurrent nature of Engine.IO,
+	// we should use a mutex to ensure the Engine.IO doesn't access
+	// the parser's Add method from multiple goroutines.
+	parserMu sync.Mutex
+	parser   parser.Parser
+}
+
+// Engine.IO ID
+func (c *serverConn) ID() string {
+	return c.id
+}
+
+func newServerConn(_eio eio.Socket, creator parser.Creator) (*serverConn, *eio.Callbacks) {
+	c := &serverConn{
+		id:  _eio.ID(),
+		eio: _eio,
+
+		sockets: newServerSocketStore(),
+
+		parser: creator(),
+	}
+
+	callbacks := &eio.Callbacks{
+		OnPacket: c.onEIOPacket,
+		OnError:  c.onError,
+		OnClose:  c.onClose,
+	}
+
+	return c, callbacks
+}
+
+func (c *serverConn) onEIOPacket(packets ...*eioparser.Packet) {
+	c.parserMu.Lock()
+	defer c.parserMu.Unlock()
+
+	for _, packet := range packets {
+		if packet.Type == eioparser.PacketTypeMessage {
+			err := c.parser.Add(packet.Data, c.onFinishEIOPacket)
+			if err != nil {
+				c.onError(err)
+				return
+			}
+		}
+	}
+}
+
+func (c *serverConn) onFinishEIOPacket(header *parser.PacketHeader, eventName string, decode parser.Decode) {
+	if header.Namespace == "" {
+		header.Namespace = "/"
+	}
+
+	if header.Type == parser.PacketTypeConnect {
+		c.connect(header, decode)
+	} else {
+		socket, ok := c.sockets.Get(header.Namespace)
+		if !ok {
+			return
+		}
+		socket.onPacket(header, eventName, decode)
+	}
+}
+
+func (c *serverConn) connect(header *parser.PacketHeader, decode parser.Decode) {
+
+}
+
+func (c *serverConn) onError(err error) {
+
+}
+
+func (c *serverConn) onClose(reason string, err error) {
+
+}
--- socket_server.go
+++ socket_server.go
@@ -1,6 +1,8 @@
 package sio
 
 import (
+	"fmt"
+	"reflect"
 	"sync"
 
 	eio "github.com/tomruk/socket.io-go/engine.io"
@@ -9,52 +11,50 @@ import (
 )
 
 type serverSocket struct {
-	id  string
-	eio eio.Socket
+	id string
 
-	// This mutex is used for protecting parser from concurrent calls.
-	// Due to the modular and concurrent nature of Engine.IO,
-	// we should use a mutex to ensure the Engine.IO doesn't access
-	// the parser's Add method from multiple goroutines.
-	parserMu sync.Mutex
-	parser   parser.Parser
+	conn *serverConn
+
+	emitter *eventEmitter
+
+	acks   map[uint64]*ackHandler
+	ackID  uint64
+	acksMu sync.Mutex
 }
 
-func newServerSocket(_eio eio.Socket, creator parser.Creator) (*serverSocket, *eio.Callbacks, error) {
+func newServerSocket(c *serverConn) (*serverSocket, error) {
 	id, err := eio.GenerateBase64ID(eio.Base64IDSize)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	s := &serverSocket{
-		id:     id,
-		eio:    _eio,
-		parser: creator(),
+		id:      id,
+		conn:    c,
+		emitter: newEventEmitter(),
 	}
 
-	callbacks := &eio.Callbacks{
-		OnPacket: s.onEIOPacket,
-		OnError:  s.onError,
-		OnClose:  s.onClose,
-	}
-
-	return s, callbacks, nil
+	return s, nil
 }
 
-func (s *serverSocket) onEIOPacket(packets ...*eioparser.Packet) {
-	s.parserMu.Lock()
-	defer s.parserMu.Unlock()
+func (s *serverSocket) onPacket(header *parser.PacketHeader, eventName string, decode parser.Decode) {
+	switch header.Type {
+	case parser.PacketTypeEvent, parser.PacketTypeBinaryEvent:
+		handlers := s.emitter.GetHandlers(eventName)
 
-	for _, packet := range packets {
-		if packet.Type != eioparser.PacketTypeMessage {
-			return
+		for _, handler := range handlers {
+			//s.onEvent(handler, header, decode)
 		}
-		s.parser.Add(packet.Data, s.onFinish)
+
+	case parser.PacketTypeAck, parser.PacketTypeBinaryAck:
+		//s.onAck(header, decode)
+
+	case parser.PacketTypeConnectError:
+		//s.onConnectError(header, decode)
+
+	case parser.PacketTypeDisconnect:
+		//s.onDisconnect()
 	}
-}
-
-func (s *serverSocket) onFinish(header *parser.PacketHeader, eventName string, decode parser.Decode) {
-
 }
 
 func (s *serverSocket) onError(err error) {
@@ -70,5 +70,77 @@ func (s *serverSocket) ID() string {
 }
 
 func (s *serverSocket) Emit(v ...interface{}) {
+	header := parser.PacketHeader{
+		Type:      parser.PacketTypeEvent,
+		Namespace: s.namespace,
+	}
 
+	if len(v) == 0 {
+		panic(fmt.Errorf("Emit: at least 1 argument expected"))
+	}
+
+	eventName := reflect.ValueOf(v)
+	if eventName.Kind() != reflect.String {
+		panic(fmt.Errorf("Emit: string expected"))
+	}
+
+	if IsEventReserved(eventName.String()) {
+		panic(fmt.Errorf("Emit: attempted to emit to a reserved event"))
+	}
+
+	f := v[len(v)-1]
+	rt := reflect.TypeOf(f)
+
+	if rt.Kind() == reflect.Func {
+		ackHandler := newAckHandler(f)
+
+		s.acksMu.Lock()
+		id := s.ackID
+		s.acks[id] = ackHandler
+		s.ackID++
+		s.acksMu.Unlock()
+
+		header.ID = &id
+
+		v = v[:len(v)-1]
+	}
+
+	buffers, err := s.parser.Encode(&header, &v)
+	if err != nil {
+		s.onError(err)
+		return
+	}
+
+	s.send(buffers...)
+}
+
+func (s *serverSocket) send(buffers ...[]byte) {
+	if len(buffers) > 0 {
+		packets := make([]*eioparser.Packet, len(buffers))
+		buf := buffers[0]
+		buffers = buffers[1:]
+
+		var err error
+		packets[0], err = eioparser.NewPacket(eioparser.PacketTypeMessage, false, buf)
+		if err != nil {
+			s.onError(err)
+			return
+		}
+
+		for i, attachment := range buffers {
+			packets[i+1], err = eioparser.NewPacket(eioparser.PacketTypeMessage, true, attachment)
+			if err != nil {
+				s.onError(err)
+				return
+			}
+		}
+
+		s.packet(packets...)
+	}
+}
+
+func (s *serverSocket) packet(packets ...*eioparser.Packet) {
+	// TODO: s.conn.packet(packets...)
+	// Instead of:
+	// go s.eio.Send(packets...)
 }
