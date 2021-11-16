3ee12e697d8e3921095069cb10968d6f53a87c60
tomruk <41700170+tomruk@users.noreply.github.com>
2021-11-16 19:42:53 +0300

    Add Socket.IO server example

---
+++ examples/server/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+	sio "github.com/tomruk/socket.io-go"
+	eio "github.com/tomruk/socket.io-go/engine.io"
+)
+
+const addr = "127.0.0.1:3000"
+
+var server *http.Server
+
+func onSocket(socket sio.Socket) {
+	fmt.Printf("New socket: %s\n", socket.ID())
+}
+
+func logEIOServerError(err error) {
+	log.Printf("Server error: %v\n", err)
+}
+
+func main() {
+	io := sio.NewServer(&sio.ServerConfig{
+		EIO: eio.ServerConfig{
+			OnError: logEIOServerError,
+		},
+	})
+
+	io.OnSocket(onSocket)
+
+	err := io.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := http.FileServer(http.Dir("public"))
+	router := mux.NewRouter()
+
+	if allowOrigin == "" {
+		// Make sure to have a slash at the end of the URL.
+		// Otherwise instead of matching with this handler, requests might match with a file that has an socket.io prefix (such as socket.io.min.js).
+		router.PathPrefix("/socket.io/").Handler(io)
+	} else {
+		if !strings.HasPrefix(allowOrigin, "http://") {
+			allowOrigin = "http://" + allowOrigin
+		}
+
+		fmt.Printf("ALLOW_ORIGIN is set to: %s\n", allowOrigin)
+		h := corsMiddleware(io, allowOrigin)
+
+		// Make sure to have a slash at the end of the URL.
+		// Otherwise instead of matching with this handler, requests might match with a file that has an socket.io prefix (such as socket.io.min.js).
+		router.PathPrefix("/socket.io/").Handler(h)
+	}
+
+	router.PathPrefix("/").Handler(fs)
+
+	fmt.Printf("Listening on: %s\n", addr)
+
+	server = &http.Server{
+		Addr:    addr,
+		Handler: router,
+
+		// It is always a good practice to set timeouts.
+		ReadTimeout: 120 * time.Second,
+		IdleTimeout: 120 * time.Second,
+
+		// HTTPWriteTimeout returns PollTimeout + 10 seconds (an extra time to write the response).
+		// You should either set this timeout to 0 (infinite) or some value greater than the PollTimeout.
+		// Otherwise poll requests may fail.
+		WriteTimeout: io.HTTPWriteTimeout(),
+	}
+
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
