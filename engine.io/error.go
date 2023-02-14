92bd2c4098f18302f39321b4615d2a9ad4db285c
tomruk <41700170+tomruk@users.noreply.github.com>
2023-02-14 20:17:27 +0300

    Add `InternalError` to engine.io

--- engine.io/error.go
+++ engine.io/error.go
@@ -1,16 +1,16 @@
-package sio
+package eio
 
-// This is a wrapper for the errors internal to socket.io.
+// This is a wrapper for the errors internal to engine.io.
 //
 // If you see this error, this means that the problem is
 // neither a network error, nor an error caused by you, but
-// the source of the error is socket.io. Open an issue on GitHub.
+// the source of the error is engine.io. Open an issue on GitHub.
 type InternalError struct {
 	err error
 }
 
 func (e InternalError) Error() string {
-	return "sio: internal error: " + e.err.Error()
+	return "eio: internal error: " + e.err.Error()
 }
 
 func (e InternalError) Unwrap() error {
