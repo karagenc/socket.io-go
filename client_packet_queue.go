51d163794fdbbece1ab19cdbd5a27b2d5762f28b
tomruk <41700170+tomruk@users.noreply.github.com>
2023-03-11 21:36:43 +0300

    Implement addToQueue

--- client_packet_queue.go
+++ client_packet_queue.go
@@ -18,8 +18,10 @@ type queuedPacket struct {
 }
 
 type clientPacketQueue struct {
+	socket *clientSocket
+
 	queuedPackets []*queuedPacket
-	socket        *clientSocket
+	mu            sync.Mutex
 }
 
 func newClientPacketQueue(socket *clientSocket) *clientPacketQueue {
@@ -31,6 +33,7 @@ func newClientPacketQueue(socket *clientSocket) *clientPacketQueue {
 func (pq *clientPacketQueue) addToQueue(header *parser.PacketHeader, v []any) {
 	haveAck := false
 	f := v[len(v)-1]
+	rv := reflect.ValueOf(f)
 	rt := reflect.TypeOf(f)
 	if rt.Kind() == reflect.Func {
 		v = v[:len(v)-1]
@@ -55,11 +58,26 @@ func (pq *clientPacketQueue) addToQueue(header *parser.PacketHeader, v []any) {
 				tryCount := packet.tryCount
 				packet.mu.Unlock()
 				if tryCount > pq.socket.config.Retries {
-
+					pq.mu.Lock()
+					pq.queuedPackets = pq.queuedPackets[1:]
+					pq.mu.Unlock()
+					if haveAck {
+						rv.Call(args)
+					}
 				}
 			} else {
-
+				pq.mu.Lock()
+				pq.queuedPackets = pq.queuedPackets[1:]
+				pq.mu.Unlock()
+				if haveAck {
+					rv.Call(args)
+				}
 			}
+			packet.mu.Lock()
+			packet.pending = false
+			packet.mu.Unlock()
+			pq.drainQueue()
+			return nil // TODO: Should this be kept nil?
 		}
 	)
 
@@ -73,7 +91,14 @@ func (pq *clientPacketQueue) addToQueue(header *parser.PacketHeader, v []any) {
 		ackF := reflect.MakeFunc(reflect.FuncOf(in, nil, false), replacementAck)
 		h = newAckHandlerWithReflectFunc(ackF, true)
 	}
+
+	pq.mu.Lock()
+	pq.queuedPackets = append(pq.queuedPackets, packet)
+	pq.mu.Unlock()
+	pq.drainQueue()
 }
+
+func (pq *clientPacketQueue) drainQueue() {}
 
 func (pq *clientPacketQueue) dismantleAckFunc(rt reflect.Type) (in, out []reflect.Type, variadic bool) {
 	in = make([]reflect.Type, rt.NumIn())
