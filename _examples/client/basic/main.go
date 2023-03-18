06c28b75a8c0c7093869a362ea6749b6292517a5
tomruk <41700170+tomruk@users.noreply.github.com>
2023-03-18 10:46:30 +0300

    Change example

--- _examples/client/basic/main.go
+++ _examples/client/basic/main.go
@@ -16,7 +16,12 @@ func main() {
 	fmt.Println("Init")
 
 	socket.OnConnect(func() {
-		fmt.Println("Connected!")
+		fmt.Printf("Connected with ID: %s\n", socket.ID())
+		fmt.Printf("Connected: %t\n", socket.Connected())
+		go func() {
+			time.Sleep(time.Second * 1)
+			socket.Emit("miyav")
+		}()
 	})
 
 	manager.OnReconnect(func(attempt uint32) {
@@ -27,13 +32,12 @@ func main() {
 		fmt.Printf("connect error: %s\n", err)
 	})
 
-	//socket.Emit("echo", "Hello!")
 	socket.OnEvent("e", func(message string) {
 		fmt.Println(message)
 	})
 
 	socket.Connect()
-	manager.Open()
+	//manager.Open()
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 500)
 }
