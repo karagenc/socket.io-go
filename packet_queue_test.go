1b3e91f2e75ea1e1deed16f1d945cb4452021b78
tomruk <41700170+tomruk@users.noreply.github.com>
2023-03-24 22:51:03 +0300

    Use subtests

--- packet_queue_test.go
+++ packet_queue_test.go
@@ -9,103 +9,105 @@ import (
 	"github.com/tomruk/socket.io-go/engine.io/parser"
 )
 
-func TestPacketQueueAddGetReset(t *testing.T) {
-	pq := newPacketQueue()
+func TestPacketQueue(t *testing.T) {
+	t.Run("add, get, and reset", func(t *testing.T) {
+		pq := newPacketQueue()
 
-	pq.add(mustCreateEIOPacket(parser.PacketTypeMessage, false, nil))
-	pq.add(mustCreateEIOPacket(parser.PacketTypePing, false, nil))
+		pq.add(mustCreateEIOPacket(parser.PacketTypeMessage, false, nil))
+		pq.add(mustCreateEIOPacket(parser.PacketTypePing, false, nil))
 
-	packets := pq.get()
-	assert.Equal(t, 2, len(packets))
-	assert.Equal(t, 0, len(pq.packets))
+		packets := pq.get()
+		assert.Equal(t, 2, len(packets))
+		assert.Equal(t, 0, len(pq.packets))
 
-	pq.add(mustCreateEIOPacket(parser.PacketTypeMessage, false, nil))
-	pq.add(mustCreateEIOPacket(parser.PacketTypePing, false, nil))
+		pq.add(mustCreateEIOPacket(parser.PacketTypeMessage, false, nil))
+		pq.add(mustCreateEIOPacket(parser.PacketTypePing, false, nil))
 
-	pq.reset()
-	assert.Equal(t, 0, len(pq.packets))
-}
+		pq.reset()
+		assert.Equal(t, 0, len(pq.packets))
+	})
 
-func TestPacketQueuePoll(t *testing.T) {
-	pq := newPacketQueue()
+	t.Run("poll", func(t *testing.T) {
+		pq := newPacketQueue()
 
-	go func() {
+		go func() {
+			pq.add(
+				mustCreateEIOPacket(parser.PacketTypeMessage, false, nil),
+				mustCreateEIOPacket(parser.PacketTypePing, false, nil),
+			)
+		}()
+
+		packets, ok := pq.poll()
+		if !assert.True(t, ok) {
+			return
+		}
+		if !assert.Equal(t, 2, len(packets)) {
+			return
+		}
+
+		// This time, synchronously add packets.
 		pq.add(
 			mustCreateEIOPacket(parser.PacketTypeMessage, false, nil),
 			mustCreateEIOPacket(parser.PacketTypePing, false, nil),
 		)
-	}()
 
-	packets, ok := pq.poll()
-	if !assert.True(t, ok) {
-		return
-	}
-	if !assert.Equal(t, 2, len(packets)) {
-		return
-	}
+		packets, ok = pq.poll()
+		if !assert.True(t, ok) {
+			return
+		}
+		if !assert.Equal(t, 2, len(packets)) {
+			return
+		}
 
-	// This time, synchronously add packets.
-	pq.add(
-		mustCreateEIOPacket(parser.PacketTypeMessage, false, nil),
-		mustCreateEIOPacket(parser.PacketTypePing, false, nil),
-	)
+		// Do reset.
+		pq.reset()
+		packets, ok = pq.poll()
+		if !assert.False(t, ok) {
+			return
+		}
+		if !assert.True(t, packets == nil) {
+			return
+		}
 
-	packets, ok = pq.poll()
-	if !assert.True(t, ok) {
-		return
-	}
-	if !assert.Equal(t, 2, len(packets)) {
-		return
-	}
+		// `alreadyDrained` should be true.
+		timedout := pq.waitForDrain(10000 * time.Second /* Forever */)
+		assert.False(t, timedout)
+	})
 
-	// Do reset.
-	pq.reset()
-	packets, ok = pq.poll()
-	if !assert.False(t, ok) {
-		return
-	}
-	if !assert.True(t, packets == nil) {
-		return
-	}
+	t.Run("waitForDrain", func(t *testing.T) {
+		pq := newPacketQueue()
 
-	// `alreadyDrained` should be true.
-	timedout := pq.waitForDrain(10000 * time.Second /* Forever */)
-	assert.False(t, timedout)
-}
+		// Artificially add packet to make `alreadyDrained` false.
+		pq.packets = append(pq.packets, mustCreateEIOPacket(parser.PacketTypeMessage, false, nil))
 
-func TestPacketQueueWaitForDrain(t *testing.T) {
-	pq := newPacketQueue()
+		timedout := pq.waitForDrain(1 * time.Millisecond)
+		assert.True(t, timedout)
+	})
 
-	// Artificially add packet to make `alreadyDrained` false.
-	pq.packets = append(pq.packets, mustCreateEIOPacket(parser.PacketTypeMessage, false, nil))
+	t.Run("pollAndSend", func(t *testing.T) {
+		pq := newPacketQueue()
+		tw := newTestWaiter(1)
 
-	timedout := pq.waitForDrain(1 * time.Millisecond)
-	assert.True(t, timedout)
-}
+		socket := eio.NewTestSocket("s1")
+		socket.SendFunc = func(packets ...*parser.Packet) {
+			assert.Equal(t, parser.PacketTypeMessage, packets[0].Type)
+			assert.Equal(t, parser.PacketTypePing, packets[1].Type)
+			assert.Equal(t, parser.PacketTypePong, packets[2].Type)
+			assert.Equal(t, parser.PacketTypeNoop, packets[3].Type)
+			tw.Done()
+		}
 
-func TestPacketQueuePollAndSend(t *testing.T) {
-	pq := newPacketQueue()
-	tw := newTestWaiter(1)
+		go pq.pollAndSend(socket)
 
-	socket := eio.NewTestSocket("s1")
-	socket.SendFunc = func(packets ...*parser.Packet) {
-		assert.Equal(t, parser.PacketTypeMessage, packets[0].Type)
-		assert.Equal(t, parser.PacketTypePing, packets[1].Type)
-		assert.Equal(t, parser.PacketTypePong, packets[2].Type)
-		assert.Equal(t, parser.PacketTypeNoop, packets[3].Type)
-		tw.Done()
-	}
+		pq.add(
+			mustCreateEIOPacket(parser.PacketTypeMessage, false, nil),
+			mustCreateEIOPacket(parser.PacketTypePing, false, nil),
+			mustCreateEIOPacket(parser.PacketTypePong, false, nil),
+			mustCreateEIOPacket(parser.PacketTypeNoop, false, nil),
+		)
 
-	go pq.pollAndSend(socket)
-
-	pq.add(
-		mustCreateEIOPacket(parser.PacketTypeMessage, false, nil),
-		mustCreateEIOPacket(parser.PacketTypePing, false, nil),
-		mustCreateEIOPacket(parser.PacketTypePong, false, nil),
-		mustCreateEIOPacket(parser.PacketTypeNoop, false, nil),
-	)
-
-	tw.Wait()
+		tw.Wait()
+	})
 }
 
 func mustCreateEIOPacket(typ parser.PacketType, isBinary bool, data []byte) *parser.Packet {
