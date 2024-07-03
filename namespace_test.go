648f81c811fafca190a5abf4ec4c6908787ca4e9
tomruk <41700170+tomruk@users.noreply.github.com>
2024-07-03 17:20:20 +0300

    Move namespace tests to their own file

---
+++ namespace_test.go
@@ -0,0 +1,82 @@
+package sio
+
+import (
+	"testing"
+
+	"github.com/tomruk/socket.io-go/internal/utils"
+)
+
+func TestNamespace(t *testing.T) {
+	t.Run("should fire a `connect` event", func(t *testing.T) {
+		io, _, manager, close := newTestServerAndClient(t, nil, nil)
+		clientSocket := manager.Socket("/", nil)
+		tw := utils.NewTestWaiter(1)
+
+		io.OnConnection(func(socket ServerSocket) {
+			tw.Done()
+		})
+		clientSocket.Connect()
+		tw.WaitTimeout(t, utils.DefaultTestWaitTimeout)
+		close()
+	})
+
+	t.Run(`should be able to equivalently start with "" or "/" on server`, func(t *testing.T) {
+		io, _, manager, close := newTestServerAndClient(t, nil, nil)
+		clientSocket := manager.Socket("/", nil)
+		tw := utils.NewTestWaiterString()
+		tw.Add("/abc")
+		tw.Add("")
+
+		io.Of("/abc").OnConnection(func(socket ServerSocket) {
+			tw.Done("/abc")
+		})
+		io.Of("").OnConnection(func(socket ServerSocket) {
+			tw.Done("")
+		})
+
+		manager.Socket("/abc", nil).Connect()
+		clientSocket.Connect()
+		tw.WaitTimeout(t, utils.DefaultTestWaitTimeout)
+		close()
+	})
+
+	t.Run(`should be equivalent for "" and "/" on client`, func(t *testing.T) {
+		io, _, manager, close := newTestServerAndClient(t, nil, nil)
+		clientSocket := manager.Socket("", nil)
+		tw := utils.NewTestWaiter(1)
+
+		io.Of("/").OnConnection(func(socket ServerSocket) {
+			tw.Done()
+		})
+
+		clientSocket.Connect()
+		tw.WaitTimeout(t, utils.DefaultTestWaitTimeout)
+		close()
+	})
+
+	t.Run("should work with `of` and many sockets", func(t *testing.T) {
+		io, _, manager, close := newTestServerAndClient(t, nil, nil)
+		clientSocket := manager.Socket("/", nil)
+		tw := utils.NewTestWaiterString()
+		tw.Add("/chat")
+		tw.Add("/news")
+		tw.Add("/")
+
+		io.Of("/chat").OnConnection(func(socket ServerSocket) {
+			tw.Done("/chat")
+		})
+		io.Of("/news").OnConnection(func(socket ServerSocket) {
+			tw.Done("/news")
+		})
+		io.OnConnection(func(socket ServerSocket) {
+			tw.Done("/")
+		})
+
+		manager.Socket("/chat", nil).Connect()
+		manager.Socket("/news", nil).Connect()
+		clientSocket.Connect()
+
+		tw.WaitTimeout(t, utils.DefaultTestWaitTimeout)
+		close()
+	})
+}
